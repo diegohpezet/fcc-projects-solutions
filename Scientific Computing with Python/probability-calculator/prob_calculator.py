7479c08cd3eb6360e56520beafceb3c2acf7f7a3
Diego Pezet <74683374+diegohpezet@users.noreply.github.com>
2024-02-09 20:27:17 -0300

    Upload probability-calculator project solution

--- Scientific Computing with Python/probability-calculator/prob_calculator.py
+++ Scientific Computing with Python/probability-calculator/prob_calculator.py
@@ -1,9 +1,32 @@
 import copy
 import random
-# Consider using the modules imported above.
 
 class Hat:
-    pass
+    def __init__(self, **kwargs):
+        self.contents = [colour for colour, amount in kwargs.items() for _ in range(amount)]
+
+    def draw(self, balls_to_draw):
+        if balls_to_draw > len(self.contents):
+            return self.contents
+        
+        drawn_balls = []
+        for _ in range(balls_to_draw):
+            random_choice = random.randint(0, len(self.contents) - 1)
+            removed_ball = self.contents.pop(random_choice)
+            drawn_balls.append(removed_ball)
+        
+        return drawn_balls
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
+    successful_experiments = 0
+    for _ in range(num_experiments):
+        hat_copy = copy.deepcopy(hat)
+        this_draw = hat_copy.draw(num_balls_drawn)
+        success = True
+        for key, value in expected_balls.items():
+            if this_draw.count(key) < value:
+                success = False
+                break
+        if success:
+            successful_experiments += 1
+    return successful_experiments/num_experiments
